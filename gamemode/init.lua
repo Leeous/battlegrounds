5d96890fe8786c7d3da85a5c15ad4837ed807bdb
Leeous <contact@leethecoder.com>
2017-07-27 15:31:33 -0400

    Add 'team switch' menu, add basic sccoreboard

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,12 +1,57 @@
 util.AddNetworkString( "popupHelp" )
+util.AddNetworkString( "popupTeamSelect" )
+util.AddNetworkString( "changeTeam" )
 
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
 
+function GM:PlayerInitialSpawn( ply )
+	ply:SetWalkSpeed( Settings.DefaultWalkSpeed )
+	ply:SetRunSpeed( Settings.DefaultRunSpeed )
+	net.Start( "popupTeamSelect" )
+	net.WriteEntity(ply)
+	net.Send(ply)
+end
+
+function GM:PlayerSpawn ( ply )
+	ply:AllowFlashlight( true )
+	ply:Give( "weapon_crowbar" )
+	ply:SetModel( "models/player/Group02/male_06.mdl" )
+	if ply:Team() != 1 then
+		ply:KillSilent()
+	end
+end
+
+function GM:PlayerDeathThink( ply )
+	if ply:Team() != 1 then
+		return false
+	else
+		ply:Spawn()
+		return true
+	end
+end
+
 function GM:ShowHelp( ply )
 	net.Start( "popupHelp" )
 	net.WriteEntity(ply)
 	net.Send(ply)
 end
+
+function GM:ShowTeam( ply )
+	net.Start( "popupTeamSelect" )
+	net.WriteEntity(ply)
+	net.Send(ply)
+end
+
+net.Receive( "changeTeam", function()
+	local ply = net.ReadEntity()
+	local teamNum = net.ReadUInt( 1 )
+	ply:SetTeam( teamNum )
+	if teamNum != 1 then
+		ply:Spectate( 4 )
+	else
+		ply:Spawn()
+	end
+end )
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,6 +3,15 @@ GM.Author = "Leeous"
 GM.Email = "contact@leeous.com"
 GM.Website = "http://leeous.com"
 
+Settings = {
+	DefaultWalkSpeed = 175,
+	DefaultRunSpeed = 275,
+	RoundTime = 20, -- Value in minutes
+}
+
 function GM:Initialize()
-	-- Do stuff
 end
+
+-- Setup the teams
+team.SetUp( 1, "Player", Color( 125, 125, 125, 255 ) )
+team.SetUp( 2, "Spectator", Color( 125, 125, 125, 255 ) )
